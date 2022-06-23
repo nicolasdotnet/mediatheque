d03188b2820202251dbeec5cdebb0e38790386f6
nicolasdotnet <nicolas.desdevises@gmail.com>
2022-06-23 12:23:41 +0200

    chore: creation of the tree structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './commons/nav-bar/nav-bar.component';
+import { FooterComponent } from './commons/footer/footer.component';
+import { NotFoundComponent } from './commons/not-found/not-found.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductsPageComponent } from './pages/products-page/products-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    NotFoundComponent,
+    HomePageComponent,
+    ProductsPageComponent
   ],
   imports: [
     BrowserModule,
